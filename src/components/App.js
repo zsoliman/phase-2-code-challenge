8ffd08910b567a057e6683e3bfe08209f463ae2e
ihollander <ianholla@gmail.com>
2020-12-05 09:15:26 -0500

    Fixed imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import Header from './/Header'
-import RandomButton from './/RandomButton'
-import PlaneteersContainer from './/PlaneteersContainer'
-import SearchBar from './/SearchBar'
+import Header from './Header'
+import RandomButton from './RandomButton'
+import PlaneteersContainer from './PlaneteersContainer'
+import SearchBar from './SearchBar'
 
 function App() {
 
